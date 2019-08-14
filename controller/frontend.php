398b59c901954b85d4c48fc22ee6b08c3d0a259b
Tommy Lutz <ltommy@hotmail.fr>
2019-08-14 13:23:35 +0200

    creation de postView

--- controller/frontend.php
+++ controller/frontend.php
@@ -6,3 +6,11 @@ function listEpisodes()
     $episodes = getEpisodes();
     require('view/listEpisodeView.php');   
 }
+
+function post()
+{
+    $post = getPost($_GET['id']);
+    $comments = getComments($_GET['id']);
+
+    require('view/postView.php');
+}
--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 require('controller/frontend.php');
 
+try {
+    if(isset($_GET['action'])){
+        if($_GET['action'] == 'listEpisodes'){
+            listEpisodes();
+        }
+        elseif($_GET['action'] == 'episode'){
+            if(isset($_GET['id']) && $_GET['id']>0){
+                episode();
+            }
+        }
+    }
+}
+
 listEpisodes();
