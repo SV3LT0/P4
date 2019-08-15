2cf65ed8aac36ab8f6bda78d3532744a9b7c4d53
Tommy Lutz <ltommy@hotmail.fr>
2019-08-15 16:39:09 +0200

    Creation getEpisode et getComments

--- model/model.php
+++ model/model.php
@@ -13,3 +13,21 @@ function dbConnect()
     return $db;
 }
 
+function getEpisode($episodeId)
+{
+    $db = dbConnect();
+    $req = $db ->query('SELECT id, titre, contenu, DATE_FORMAT(creation_date, "%d/%m/%Y")AS creation_date_fr FROM episode WHERE id = ?');
+    $req->execute(array($episodeId));
+    $episode = $req->fetch();
+
+    return $episode
+}
+
+function getComments($episodeId)
+{
+    $db = dbConnect();
+    $comments = $db->prepare('SELECT id, auteur, contenu DATE_FORMAT(dateComm, "%d/%m/%Y à %Hh%imin%ss")AS comment_date FROM commentaire WHERE idEpisode = ? ORDER BY dateComm DESC');
+    $comments->execute(array($episodeId));
+
+    return $comments;
+}
