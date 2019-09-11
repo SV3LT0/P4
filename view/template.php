a3341fa77f6254e99fce02c76b6afcd5b951ea34
Tommy Lutz <ltommy@hotmail.fr>
2019-09-11 13:10:41 +0200

    css avec police et responsive

--- view/template.php
+++ view/template.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        
+        <meta name="viewport" content="width=device-width" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href="public/style.css" rel="stylesheet" />
         <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
@@ -16,7 +16,7 @@
 
     <body>
         <header id='header'>
-            <h1>Billet simple pour l'Alaska</h1>
+            <h1>Billet Simple Pour l'Alaska</h1>
                 <?php
                     if(isset($_SESSION['pseudo'])){ ?>
                         <div class='compte'>
